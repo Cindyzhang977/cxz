371fa690e862cd89cc578bdbd8a6b03f5993121c
Cindy Zhang <cindyzhang977@gmail.com>
2021-01-12 13:21:56 -0800

    add cs61b website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/custom-styles.scss'
 import './styles/app.css'
@@ -12,33 +13,48 @@ import Home from './Home.js'
 import Projects from './Projects.js'
 import About from './About.js'
 import Experience from './Experience.js'
+import CS61B from './CS61B.js'
 
 function App() {
 
   return (
     <div id='home'>
       <NavBar />
-      <div className='body'>
-        <Home />
-        <h1 id='projects'>projects</h1>
-        <Projects />
-        <h1 id='experience'>experience</h1>
-        <Experience />
-        <h1 id='about'>about</h1>
-        <About />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <div className='body'>
+              <Home />
+              <h1 id='projects'>projects</h1>
+              <Projects />
+              <h1 id='experience'>experience</h1>
+              <Experience />
+              <h1 id='about'>about</h1>
+              <About />
+            </div>
+          </Route>
+          <Route path="/cs61b">
+            <CS61B />
+          </Route>
+        </Switch>
+      </Router>
       <Footer />
     </div>
   );
 }
 
 function NavBar() {
+  const is61b = window.location.pathname.includes("cs61b")
+
+  console.log(is61b)
+  console.log(window.location.pathname)
+
   return (
     <Navbar collapseOnSelect expand='lg' bg='navy' sticky='top' variant='dark' className='navbar'>
-    <Navbar.Brand href='#home'><div className='navlink'>Cindy Zhang</div></Navbar.Brand>
-    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+    <Navbar.Brand href='/'><div className='navlink'>Cindy Zhang</div></Navbar.Brand>
+    <Navbar.Toggle aria-controls='responsive-navbar-nav' className={is61b ? 'hide' : ''} />
       <Navbar.Collapse id='responsive-navbar-nav'>
-        <Nav className='ml-auto'>
+        <Nav className={'ml-auto' + (is61b ? ' hide' : '')}>
           <Nav.Link href='#home' className='navlink'>Home</Nav.Link>
           <Nav.Link href='#projects' className='navlink'>Projects</Nav.Link>
           <Nav.Link href='#experience' className='navlink'>Experience</Nav.Link>
---
+++ src/CS61B.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/cs61b.css'
+import { Container } from 'react-bootstrap'
+
+function CS61B() {
+  const discussions = [
+    // {
+    //   name: ,
+    //   link: 
+    // }
+  ]
+
+  const labs = [
+    // {
+    //   name: ,
+    //   link:
+    // }
+  ]
+
+  const guides = [
+    {
+      name: "Object Oriented Programming",
+      link: "https://github.com/Cindyzhang977/cs61b-teaching/blob/main/object-oriented-programming.md"
+    },
+    {
+      name: "Asymptotics",
+      link: "https://github.com/Cindyzhang977/cs61b-teaching/blob/main/asymptotics.md"
+    },
+    {
+      name: "Hashing",
+      link: "https://github.com/Cindyzhang977/cs61b-teaching/blob/main/hashing.md"
+    },
+    {
+      name: "Graphs",
+      link: "https://github.com/Cindyzhang977/cs61b-teaching/blob/main/graphs.md"
+    },
+    {
+      name: "Sorting",
+      link: "https://github.com/Cindyzhang977/cs61b-teaching/blob/main/sorting.md"
+    }
+  ]
+
+  return (
+    <Container className="cs61b-container">
+      <div id="greeting">
+        welcome to CS 61B!
+      </div>
+      <div>
+        Below are my teaching materials for CS61B. Feel free to reach out to me 
+        at <a href="mailto:cindyxzhang@berkeley.edu" target="_blank" rel="noopener noreferrer">cindyxzhang@berkeley.edu</a> if
+        you have any questions. 
+      </div>
+      <Container className="section">
+        <div className="header">
+          Discussions
+        </div>
+        <div>
+          Discussion slides and other materials will be posted here every week.
+        </div>
+        <ul className="links">
+          {
+            discussions.map((disc, index) => {
+              return (
+                <li key={index}>
+                  <a href={disc.link}>{disc.name}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </Container>
+      <Container className="section">
+        <div className="header">
+          Labs
+        </div>
+        <div>
+          Lab slides and other materials will be posted here every week.
+        </div>
+        <ul className="links">
+          {
+            labs.map((lab, index) => {
+              return (
+                <li key={index}>
+                  <a href={lab.link}>{lab.name}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </Container>
+      <Container className="section">
+        <div className="header">
+          Resources
+        </div>
+        <div>
+          These guides are for some of the most important concepts taught in CS61B and is definitely not comprehensive 
+          of the entire course. They are not meant to substitute lecture, but rather to supplement it. As a result, some 
+          familiarity with the material is assumed. For best results, read after watching lecture.
+        </div>
+        <ul className="links">
+          {
+            guides.map((guide, index) => {
+              return (
+                <li key={index}>
+                  <a href={guide.link}>{guide.name}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </Container>
+    </Container>
+  )
+}
+
+export default CS61B
